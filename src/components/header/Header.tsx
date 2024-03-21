2689b322c12fd50b139aeb9edb750c1f818e5c93
Maxim Predybailo <predmaxim@ya.ru>
2024-03-21 23:21:39 +0500

    feat(front): change some Header component logic and styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,34 @@
+'use client';
 import styles from './Header.module.scss';
-import {Menu} from '@/components';
+import {Button, Menu} from '@/components';
 import headerMenu from '@/mocks/menu.json';
-import {MenuItemType} from '@/types/globals';
+import {useAppSelector} from '@/redux/hooks';
+import {PiUserCircleFill} from 'react-icons/pi';
+import Link from 'next/link';
 
 export const Header = () => {
+  const {email, isLogged} = useAppSelector((state) => state.user.value);
   return (
     <header className={styles.Header}>
       <div className={styles.Header__menu}>
-        <Menu menuItems={headerMenu as unknown as MenuItemType[]}/>
+        <Menu menuItems={headerMenu}/>
+        {isLogged
+          ? <Link
+            href="/profile"
+            className={styles.profileBtn}
+          >
+            <PiUserCircleFill/> {email.split('@')[0]}
+          </Link>
+          : <Button
+            size="small"
+            tag="a"
+            href="/login"
+            className={styles.signInBtn}
+          >
+            Sign In
+          </Button>
+        }
+
       </div>
     </header>
   );
